a0375ed0c2faa2fe232deea3db25742f7fc7e23c
Celio Iyama <celio.iyama@gmail.com>
2021-07-19 13:08:10 -0300

    Demo - inserir dados parte 2 - Java

--- jdbc3/src/application/Program.java
+++ jdbc3/src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -17,28 +19,47 @@ public class Program {
 		PreparedStatement st = null;
 		try {
 			conn= DB.getConnection();
-			
+			/*
 			st = conn.prepareStatement(
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 					+ " Values "
-					+ "(?, ?, ?, ?, ?)"
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS
 					);
 			st.setString(1, "Carl Purple");
 			st.setString(2, "carl@gmail.com");
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
+			*/
+			st = conn.prepareStatement(
+					"insert into department (name) values ('D1'), ('D2')",
+					Statement.RETURN_GENERATED_KEYS
+					);
+			
 			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id= " + id);
+				}
+			}
+			else {
+				System.out.println("No rows affected!");
+			}
 			
 			System.out.println( "Done! Rows affected: " + rowsAffected);
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
-		catch (ParseException e) {
-			e.printStackTrace();
-		}
+		//catch (ParseException e) {
+		//	e.printStackTrace();
+		//}
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
